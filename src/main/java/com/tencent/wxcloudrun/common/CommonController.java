6de5446e286ebdd45978b19957e983aabe182941
zhangping <592634988@qq.com>
2025-08-22 16:48:55 +0800

    feat:v1

--- src/main/java/com/tencent/wxcloudrun/common/CommonController.java
+++ src/main/java/com/tencent/wxcloudrun/common/CommonController.java
@@ -1,11 +1,13 @@
 package com.tencent.wxcloudrun.common;
 
+import com.alibaba.fastjson.JSONObject;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 @RestController
 @Slf4j
