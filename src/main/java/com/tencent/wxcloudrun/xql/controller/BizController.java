73f36307aa3f93dcb050deb1a8eef96004fd0ac7
zhangping <592634988@qq.com>
2025-08-22 17:22:48 +0800

    feat:v1

--- src/main/java/com/tencent/wxcloudrun/xql/controller/BizController.java
+++ src/main/java/com/tencent/wxcloudrun/xql/controller/BizController.java
@@ -32,8 +32,8 @@ public class BizController {
      *
      * @return API response json
      */
-    @GetMapping(value = "/api/count1")
-    ApiResponse get() {
+    @GetMapping(value = "/api/count")
+    ApiResponse query() {
         logger.info("/api/count get request");
         Optional<Xql> counter = bizService.getCounter(1);
         String count = null;
@@ -42,39 +42,6 @@ public class BizController {
         }
 
         return ApiResponse.ok(count);
-    }
-
-
-    /**
-     * 更新计数，自增或者清零
-     *
-     * @param request {@link CounterRequest}
-     * @return API response json
-     */
-    @PostMapping(value = "/api/count1")
-    ApiResponse create(@RequestBody CounterRequest request) {
-        logger.info("/api/count post request, action: {}", request.getAction());
-
-        Optional<Xql> curCounter = bizService.getCounter(1);
-        if (request.getAction().equals("inc")) {
-            String count = "1";
-            if (curCounter.isPresent()) {
-                count += curCounter.get().getAccount();
-            }
-            Xql counter = new Xql();
-            counter.setId(1);
-            counter.setAccount(count);
-            bizService.upsertCount(counter);
-            return ApiResponse.ok(count);
-        } else if (request.getAction().equals("clear")) {
-            if (!curCounter.isPresent()) {
-                return ApiResponse.ok(0);
-            }
-            bizService.clearCount(1);
-            return ApiResponse.ok(0);
-        } else {
-            return ApiResponse.error("参数action错误");
-        }
     }
 
 }
