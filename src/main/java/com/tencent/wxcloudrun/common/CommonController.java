f2ae9e746ffaf4def3841a3fd9fbabe99d32630c
zhangping <592634988@qq.com>
2025-08-26 09:17:10 +0800

    feat:14

--- src/main/java/com/tencent/wxcloudrun/common/CommonController.java
+++ src/main/java/com/tencent/wxcloudrun/common/CommonController.java
@@ -4,10 +4,16 @@ import com.alibaba.fastjson.JSONObject;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.HashMap;
@@ -17,16 +23,25 @@ import java.util.Map;
 @Slf4j
 public class CommonController {
     @PostMapping("/msg/rece")
-    public ApiResponse getMsg(@RequestBody JSONObject request) {
-        log.error("getMsg,{}", request.toString());
+    public ApiResponse getMsg(@RequestBody JSONObject jsonObject, HttpRequest request) {
+        log.error("getMsg,{}", jsonObject.toString());
         // {"Content":"1","CreateTime":1755854772,"ToUserName":"gh_d17c82863523","FromUserName":"oe9UKt6zlWd9hHk8S79ggqXJsqpY","MsgType":"text","MsgId":25137798904747621}
-        String content = request.getString("Content");
+        String content = jsonObject.getString("Content");
         if (StringUtils.isBlank(content)) {
             return ApiResponse.error("消息为空");
         }
+
+        Header[] headers = request.getHeaders("X-WX-CLOUDBASE-ACCESS-TOKEN");
+        if (headers == null || headers.length < 1) {
+            return ApiResponse.error("未获取到请求头");
+        }
+        String value = headers[0].getValue();
+        if (StringUtils.isBlank(value)) {
+            return ApiResponse.error("未获取到请求头");
+        }
         Map<String, Object> payload = new HashMap<>(16);
-        payload.put("FromUserName", request.getString("ToUserName"));
-        payload.put("ToUserName", request.getString("FromUserName"));
+        payload.put("FromUserName", jsonObject.getString("ToUserName"));
+        payload.put("ToUserName", jsonObject.getString("FromUserName"));
         payload.put("CreateTime", LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8")));
         payload.put("MsgType", "text");
         if (content.contains("学企来")) {
@@ -66,12 +81,22 @@ public class CommonController {
         try {
             log.error("回复消息,{}", JSONObject.toJSONString(payload));
             // 被动回复
-            HttpUtils.doPost("http://api.weixin.qq.com", "/cgi-bin/message/custom/send", null, null, payload);
+            HttpResponse response = HttpUtils.doPost("http://api.weixin.qq.com", "/cgi-bin/message/custom/send?cloudbase_access_token=" + value, null, null, payload);
+            try (InputStream content1 = response.getEntity().getContent()) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content1));
+                String line;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                log.error("回复消息结果{}", stringBuilder);
+            }
+
         } catch (Exception e) {
             log.error("回复消息失败{}", e.getMessage());
             return ApiResponse.error("回复消息失败");
         }
-//        payload.put("Content", "测试接口返回值会不会触发回复！！" + request.getString("FromUserName"));
-        return ApiResponse.ok(payload);
+//        payload.put("Content", "测试接口返回值会不会触发回复！！" + jsonObject.getString("FromUserName"));
+        return ApiResponse.ok();
     }
 }
